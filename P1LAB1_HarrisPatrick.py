266f4d3c22fe0ff4d9f70c82169e5ee010e994dd
harrisp3138 <harrisp3138@student.faytechcc.edu>
2025-02-05 11:24:24 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_HarrisPatrick.py
@@ -0,0 +1,10 @@
+# Patrick Harris
+# 2/5/25
+# P1LAB1
+# How to get input from a user and display output
+
+firstName = input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+
+print('Hello,', firstName, lastName + '!', 'Welcome to CTI-110')
+
